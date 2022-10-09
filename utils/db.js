af1d72ecaa76005146b22411fceec94ee895fb44
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2022-10-09 15:16:12 +0800

    Menambahkan URL

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 
 const mongoose = require("mongoose");
-const url = process.env.MONGODB_URI;
+// const url = process.env.MONGODB_URI;
+const url =
+  "mongodb+srv://DwiWahyu:DeadlyMamba89@cluster0.jpu1d.mongodb.net/?retryWrites=true&w=majority";
 
 function connectDB() {
   mongoose.connect(url, (err) => {
